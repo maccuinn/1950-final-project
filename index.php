88a3124c15257239c065ab8449af23bc70ce0c81
maccuinn <april.l.hall@gmail.com>
2018-03-24 18:52:24 -0700

    added html markup for home page

--- index.php
+++ index.php
@@ -1,13 +1,89 @@
 <!DOCTYPE html>
 <html lang="en">
     <?php 
-        include '/templates/head.php';
+        include 'templates/head.php';
     ?>
 <body>
     <?php
-        include '/templates/header.php';
+        include 'templates/header.php';
     ?>
     <main>
+        <div class="banner">
+            <img src="/images/student-main-banner.jpg" alt="Three Students Sitting On Grass Looking At Laptops">
+        </div>
+
+        <section class="intro">
+            <h2>Launch Your New Career</h2>
+            <p>With Web Development And Design 2</p>
+
+            <div class="skills">
+                <h3>Hands On Learning of In Demand Skills</h3>
+                <ul>
+                    <li>HTML5</li>
+                    <li>CSS3</li>
+                    <li>Media Queries</li>
+                    <li>JQuery</li>
+                </ul>
+                <ul>
+                    <li>SSI</li>
+                    <li>Sass</li>
+                    <li>Git and Github</li>
+                    <li>SEO</li>
+                </ul>
+                <a href="#0">Learning Outcomes</a>
+            </div>
+
+            <div class="advantage">
+                <h3>The BCIT Advantage</h3>
+                <ul>
+                    <li>Small Class Sizes</li>
+                    <li>Hands On Learning...Every Class</li>
+                    <li>Taught By Industry Professionals</li>
+                    <li>Work In a Team To Create a Functional Website</li>
+                </ul>
+                <a href="#0">Learn More</a>
+            </div>
+
+            <div class="secondary-nav">
+                <h2>About This Course</h2>
+                <ul>
+                    <li><a href="#0"></a>Course Details</li>
+                    <li><a href="#0"><Learning Outcomes/a></li>
+                    <li><a href="#0"></a>Course Schedule</li>
+                    <li><a href="#0"></a>Lessons</li>
+                </ul>
+                <h2>Registration Info</h2>
+                <ul>
+                    <li><a href="#0">Registration Info</a></li>
+                    <li><a href="#0"></a>Talk With An Advisor</li>
+                    <li><a href="#0">Contact Us</a></li>
+                </ul>
+            </div>
+        </section>
+
+        <section class="employment">
+            <h2>96% Of BCIT Grads Are Employed</h2>
+            <p>Prepare yourself for a career in web. Take one course, or apply this course to a part-time certificate. Work at your own pace, in the evenings or weekends. A Flexible schedule that fits your lifestyle.</p>
+            <a href="#0">Student Outcomes</a>
+        </section>
+
+        <section class="description">
+        <ul>
+            <li>Mark up pages using latest HTML technologies, including HTML5.</li>
+            <li>Utilize CSS to control page layout and presentation, including CSS3.</li>
+            <li>Add behaviour to any web page using JavaScript.</li>
+            <li>Create dynamic content using JavaScript frameworks like jQuery.</li>
+            <li>Employ Web Standards when building web sites for maximum compatibility.</li>
+            <li>Understand concepts for Search Engine Optimized content (SEO).</li>
+            <li>Implement Server Side Includes for efficient web development.</li>
+            <li>Create template-driven pages for use in Content Management systems.</li>
+            <li>Understand basic development methodology.</li>
+            <li>Use media queries to create Responsive designs for mobile and desktop presentation.</li>
+            <li>Use HTML5 technologies to present rich content such as video, audio and geolocation.</li>
+            <li>Create professional looking typography using web-based fonts.</li>
+            <li>Understand source control using GIT.</li>
+        </ul>
+        </section>
 
     </main>
     <?php
