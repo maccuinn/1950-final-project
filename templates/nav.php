1376cc66507f6b9a876915c1bb8607009f801e98
maccuinn <april.l.hall@gmail.com>
2018-03-25 21:43:33 -0700

    added navigation paths to main nav

--- templates/nav.php
+++ templates/nav.php
@@ -1,9 +1,10 @@
 <nav>
     <ul>
-        <li><a href="#0">Outline</a></li>
-        <li><a href="#0">Schedule</a></li>
-        <li><a href="#0">Lessons</a></li>
-        <li><a href="#0">Contact</a></li>
+        <li><a href="../index.php">Home</a></li>
+        <li><a href="../outline.php">Outline</a></li>
+        <li><a href="../schedule.php">Schedule</a></li>
+        <li><a href="../lessons.php">Lessons</a></li>
+        <li><a href="../contact.php">Contact</a></li>
         <li class=login><a href="#0">Login</a></li>
     </ul>
 </nav>
