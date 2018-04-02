78c1d2ef2c9043c63e8e6aecee6e22f818960583
maccuinn <april.l.hall@gmail.com>
2018-04-01 23:10:03 -0700

    Finished desktop version of contact page

--- contact.php
+++ contact.php
@@ -12,6 +12,31 @@
         ?>
     </div>
 
+    <div class="contact-page">
+        <img src="/images/coffee-cropped.jpg" alt="Woman looking accross the street with a blurred background.">
+        <div class="contact-info">
+            <h2>Give Us A Shout</h2>
+            <div class="instructor-info">
+                <h3>Contact Information</h3>
+                <p>Instructor: Michael Whyte</p>
+                <p>Email: mywhyte13@learn.bcit.ca</p>
+            </div>
+        </div>
+        <section class="contact-form">
+            <div class="form-container">
+                <form onsubmit="document.forms[0].checkValidity() && alert('Thank You. Your request has been sent.')">
+                    <label for="radio-1">Current Student </label><input id="radio-1" type="radio" name="student" value="current" required>
+                    <label for="radio-2">Prospective Student </label><input id="radio-2" type="radio" name="student" value="prospective" required>
+                    <input type="text" name="fname" placeholder="first name*" required>
+                    <input type="text" name="snumber" placeholder="student number" pattern="^A/d{8}$">
+                    <input type="text" name="lname" placeholder="last name*" required>
+                    <input type="email" name="email" placeholder="email*" required>
+                    <textarea name="message" rows="5" placeholder="what can we help you with*" required></textarea>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        </section>
+    </div>
     </main>
     <?php
         include 'templates/footer.php';
